9d3a1702e417d08c39fe98f38980b0f072bd987f
SylSesay <33230493+SylSesay@users.noreply.github.com>
2019-03-05 21:54:29 -0500

    Add files via upload

---
+++ TweetViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Sylvia Sesay on 2/28/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TweetTextView.becomeFirstResponder()
+    }
+    
+    @IBOutlet weak var TweetTextView: UITextView!
+    
+    @IBAction func Cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func Tweet(_ sender: Any) {
+        if(!TweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: TweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
